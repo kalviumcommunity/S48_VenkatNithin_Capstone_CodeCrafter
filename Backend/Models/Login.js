d406e1b0c1c0c21a3379d073d1d9eb7b3daca022
Venkat-Nithin <venkatnithinm@gmail.com>
2024-09-10 14:18:20 +0530

    Database Schema created

---
+++ Backend/Models/Login.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const loginSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+const Login = mongoose.model('Login', loginSchema);
+
+module.exports = Login;
---
+++ Backend/Models/Signup.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const signupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+const Signup = mongoose.model('Signup', signupSchema);
+
+module.exports = Signup;
